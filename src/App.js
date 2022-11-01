55d2b6d61edbb2207c438f87f0ce55991ac3483a
1010varun <varunagarwal10.2@gmail.com>
2022-11-01 15:59:24 +0530

    add: counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from "react";
+import "./App.css"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {count : 0};
+
+const reducer = (state, action) => {
+  const {type} = action;
+  if(type === "increment"){
+    console.log("incerment");
+    return({...state, count : state.count + 1});
+  }
+  if(type === "decrement"){
+    console.log("decrement");
+    return({count :state.count-1});
+  }
+  if(type === "reset"){
+    console.log("reset");
+    return({...state, count: 0});
+  }
+  else {
+    throw new Error("option not found");
+  }
 }
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return(
+    <div className="App-header">
+      <article>count : {state.count}</article>
+      <button className="btn bg-primary" onClick={() => {dispatch({type: "increment"})}}>increment</button>
+      <button className="btn bg-primary" onClick={() => {dispatch({type: "decrement"})}}>decrement</button>
+      <button className="btn bg-primary" onClick={() => dispatch({type: "reset"})}>reset</button>
+    </div>
+  )
+}
+
 
 export default App;
